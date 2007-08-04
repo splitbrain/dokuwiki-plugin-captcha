5a7dc179cf9b3f439abf9f71e99cbdbe30c7b20d
Andreas Gohr <andi@splitbrain.org>
2007-08-04 09:32:30 +0200

    German Translation
    darcs-hash:20070804073230-7ad00-e0e9024a79912c8ac02fd1aab181a913bb8a33cb.gz

---
+++ lang/de/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * German language file
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['testfailed']  = "Das CAPTCHA wurde nicht korrekt beantwortet.";
+$lang['fillcaptcha'] = "Bitte übertragen Sie die Buchstaben in das Eingabefeld.";
+$lang['soundlink']   = "Wenn Sie die Buchstaben auf dem Bild nicht lesen können, laden Sie diese .wav Datei herunter, um sie vorgelesen zu bekommen.";
+
---
+++ lang/de/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * German language file
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['mode']       = "Welcher CAPTCHA-Typ soll benutzt werden?";
+$lang['mode_o_js']    = "Text (automatisch ausgefüllt via JavaScript)";
+$lang['mode_o_text']  = "Text (manuell auszufüllen)";
+$lang['mode_o_image'] = "Bild (nicht barrierefrei)";
+$lang['mode_o_audio'] = "Bild+Audio (barrierefrei)";
+
+$lang['regprotect'] = "Protect the registration form as well?";
+$lang['forusers']   = "Use CAPTCHA for logged in users, too?";
+$lang['width']      = "Width of the CAPTCHA image (pixel)";
+$lang['height']     = "Height of the CAPTCHA image (pixel)";
+
