d8e5a149d8c0e3b700e8db51ba299b66f3173cca
Andreas Gohr <andi@splitbrain.org>
2007-08-04 08:17:27 +0200

    Slovak Translation
    darcs-hash:20070804061727-7ad00-b08593ded3629fbec1eabcd89b06ae837228b262.gz

---
+++ lang/sk/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Slovak language file
+ *
+ * @author Jozef Riha <jose1711@gmail.com>
+ */
+
+$lang['testfailed']  = "Ľutujem, ale na CAPTCHA nebolo odpovedané správne. Je možné, že by ste vôbec neboli človekom?";
+$lang['fillcaptcha'] = "Vyplňte prosím všetky písmená v poli, aby ste dokázali, že nie ste skript.";
+$lang['soundlink']   = "Ak nedokážete prečítať písmená na obrázku, stiahnite si tento .wav súbor a text vám prečítame.";
+
---
+++ lang/sk/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Slovak language file
+ *
+ * @author Jozef Riha <jose1711@gmail.com>
+ */
+
+$lang['mode']       = "Ktorý typ CAPTCHA sa má použiť?";
+$lang['mode_o_js']    = "Text (predvyplnený JavaScriptom)";
+$lang['mode_o_text']  = "Text (iba manuálne vložený)";
+$lang['mode_o_image'] = "Obrázok (zlá prístupnosť)";
+$lang['mode_o_audio'] = "Obrázok (lepšia prístupnosť)";
+
+$lang['regprotect'] = "Chrániť tiež registračný formulár?";
+$lang['forusers']   = "Používať CAPTCHA aj pre registrovaných užívateľov?";
+$lang['width']      = "Šírka CAPTCHA obrázku (v bodoch)";
+$lang['height']     = "Výška CAPTCHA obrázku (v bodoch)";
+
