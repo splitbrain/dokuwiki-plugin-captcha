0a90fb7baf7a66f72699f2af7bca7e6247ed9b48
manu <manu@seedfloyd.fr>
2007-12-10 20:13:51 +0100

    French translation
    darcs-hash:20071210191351-e367c-de1a762d53689c71009000196033eb266726933d.gz

---
+++ lang/fr/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * French language file
+ *
+ * @author Emmanuel Dupin <seedfloyd@gmail.com>
+ */
+
+$lang['testfailed']  = "Désolé mais vous n'avez pas répondu correctement au test anti-spam. Peut-être n'êtes vous pas humain ?";
+$lang['fillcaptcha'] = "Merci de recopier le code ci-contre pour prouver que vous n'êtes pas un robot:";
+$lang['soundlink']   = "Si vous ne pouvez déchiffrer le code, téléchargez ce fichier .wav pour l'entendre.";
+
---
+++ lang/fr/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * French language file
+ *
+ * @author Emmanuel Dupin <seedfloyd@gmail.com>
+ */
+
+$lang['mode']       = "Quel type de CAPTCHA utiliser ?";
+$lang['mode_o_js']    = "Texte (prérempli avec JavaScript)";
+$lang['mode_o_text']  = "Texte (remplissage manuel)";
+$lang['mode_o_image'] = "Image (peu accessible)";
+$lang['mode_o_audio'] = "Image + Audio (meilleure accessibilité)";
+
+$lang['regprotect'] = "Protégér également le formulaire d'inscription ?";
+$lang['forusers']   = "Utiliser également le CAPTCHA pour les utilisateurs connectés ?";
+$lang['width']      = "Largeur du CAPTCHA image (en pixels)";
+$lang['height']     = "Hauteur du CAPTCHA image (en pixels)";
+
