39d3fb72915b9d921c6dfe2fecb3d0c305c41419
Andreas Gohr <andi@splitbrain.org>
2008-02-15 22:57:01 +0100

    Czech language file
    darcs-hash:20080215215701-7ad00-6956f729b265c5a8677ba1eaeb55f17b7d29ca1c.gz

---
+++ lang/cs/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Czech language file
+ *
+ * @author Antonin Komenda <gree@grees.net>
+ */
+
+$lang['testfailed']  = "Bohužel, ale na CAPTCHA nebylo odpovězeno správně. Jste vůbec člověk?";
+$lang['fillcaptcha'] = "Vyplňte, prosím, všechna písmena v poli, abyste dokázali, že nejste robot.";
+$lang['soundlink']   = "Pokud nedokážete přečíst písmena na obrázku, stáhněte si tento .wav soubor, kde je text přečtený.";
+
---
+++ lang/cs/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Czech language file
+ *
+ * @author Antonin Komenda <gree@grees.net>
+ */
+
+$lang['mode']       = "Který typ CAPTCHA se má použít?";
+$lang['mode_o_js']    = "Text (předvyplněný JavaScriptem)";
+$lang['mode_o_text']  = "Text (pouze manuálně vložený)";
+$lang['mode_o_image'] = "Obrázek (špatná přístupnost)";
+$lang['mode_o_audio'] = "Obrázek (lepší přístupnost)";
+
+$lang['regprotect'] = "Chránit také registrační formulář?";
+$lang['forusers']   = "Používat CAPTCHA i pro registrované uživatele?";
+$lang['width']      = "Šírka CAPTCHA obrázku (v bodech)";
+$lang['height']     = "Výška CAPTCHA obrázku (v bodech)";
+
