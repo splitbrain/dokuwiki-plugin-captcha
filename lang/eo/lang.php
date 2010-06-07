9a3814ec5f42bfd1d265da275cc6c081d9e4f44f
Ljosxa KUZNECOV <ka2pink@gmail.com>
2010-06-07 20:46:24 +0200

    Esperanto translation added

---
+++ lang/eo/lang.php
@@ -0,0 +1,11 @@
+﻿<?php
+/**
+ * Esperanto language file
+ *
+ * @author Tradukis Ljosxa Kuznecov <ka2pink@gmail.com>
+ */
+
+$lang['testfailed']  = "Pardonon, sed CAPTCHA ne respondis korekte. Eble vi tute ne estas homo, ĉu?";
+$lang['fillcaptcha'] = "Bonvolu tajpi ĉiujn literojn en la kampeton, por pruvi ke vi estas homo.";
+$lang['soundlink']   = "Se vi ne povas legi la literojn en la bildo, ŝarĝu tiun .wav-dosieron por aŭdi ilin.";
+
---
+++ lang/eo/settings.php
@@ -0,0 +1,19 @@
+﻿<?php
+/**
+ * Esperanto language file
+ *
+ * @author Tradukis Ljosxa Kuznecov <ka2pink@gmail.com>
+ */
+
+$lang['mode']          = "Kiun varianton de CAPTCHA uzi?";
+$lang['mode_o_js']     = "Teksto (prilaborita per Java-skripto)";
+$lang['mode_o_text']   = "Teksto (nur prove)";
+$lang['mode_o_image']  = "Bildo (malbona uzebleco)";
+$lang['mode_o_audio']  = "Bildo+Sono (pli bona uzebleco)";
+$lang['mode_o_figlet'] = "Figlet ASCII - arto (malbona uzebleco)";
+
+$lang['regprotect'] = "Ĉu protekti paĝon por registriĝo?";
+$lang['forusers']   = "Uzi CAPTCHA-n ankaŭ por ensalutintaj uzantoj?";
+$lang['width']      = "Vasto de CAPTCHA-bildo (pikseloj)";
+$lang['height']     = "Alto de CAPTCHA-bildo (pikseloj)";
+
