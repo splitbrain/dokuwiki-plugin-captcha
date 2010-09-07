b6809e22357e1bc7b761bbf900686a1beb49f765
Aleksandr Selivanov <alexgearbox@gmail.com>
2010-09-07 21:29:53 +0200

    Russian translation added

---
+++ lang/ru/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+
+$lang['testfailed']  = "Извините, код подтверждения введён неверно.";
+$lang['fillcaptcha'] = "Пожалуйста, введите код подтверждения, чтобы доказать, что вы не робот:";
+$lang['soundlink']   = "Если вы не можете прочитать символы на изображении, загрузите и воспроизведите wav-файл.";
+
---
+++ lang/ru/settings.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+
+$lang['mode']          = "Какой тип CAPTCHA использовать?";
+$lang['mode_o_js']     = "Текст (заполнение JavaScript)";
+$lang['mode_o_text']   = "Текст (ручной ввод)";
+$lang['mode_o_image']  = "Изображение (хорошая защита)";
+$lang['mode_o_audio']  = "Изображение и звук (плохая защита)";
+$lang['mode_o_figlet'] = "Figlet ASCII Art (хорошая защита)";
+
+$lang['regprotect'] = "Защитить регистрационную форму?";
+$lang['forusers']   = "Использоваться CAPTCHA для зарегистрированных пользователей?";
+$lang['width']      = "Ширина изображения CAPTCHA (пиксель)";
+$lang['height']     = "Высота изображения CAPTCHA (пиксель)";
+
