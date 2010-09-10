b8bb276a1063351d1a0218a51c1155788005452a
Emmanuel Dupin <seedfloyd@gmail.com>
2010-09-10 19:59:20 +0200

    French language update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -5,7 +5,7 @@
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
  */
 
-$lang['testfailed']  = "Désolé mais vous n'avez pas répondu correctement au test anti-spam. Peut-être n'êtes vous pas humain ?";
-$lang['fillcaptcha'] = "Merci de recopier le code ci-contre pour prouver que vous n'êtes pas un robot:";
+$lang['testfailed']  = "Désolé, vous n'avez pas répondu correctement au test anti-spam.";
+$lang['fillcaptcha'] = "Merci de recopier le code ci-contre (anti-spam) :";
 $lang['soundlink']   = "Si vous ne pouvez déchiffrer le code, téléchargez ce fichier .wav pour l'entendre.";
 
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -5,13 +5,14 @@
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
  */
 
-$lang['mode']       = "Quel type de CAPTCHA utiliser ?";
-$lang['mode_o_js']    = "Texte (prérempli avec JavaScript)";
-$lang['mode_o_text']  = "Texte (remplissage manuel)";
-$lang['mode_o_image'] = "Image (peu accessible)";
-$lang['mode_o_audio'] = "Image + Audio (meilleure accessibilité)";
+$lang['mode']          = "Quel type de CAPTCHA utiliser ?";
+$lang['mode_o_js']     = "Texte (prérempli avec JavaScript)";
+$lang['mode_o_text']   = "Texte (remplissage manuel)";
+$lang['mode_o_image']  = "Image (peu accessible)";
+$lang['mode_o_audio']  = "Image + Audio (meilleure accessibilité)";
+$lang['mode_o_figlet'] = "ASCII Art (peu accessible)";
 
-$lang['regprotect'] = "Protégér également le formulaire d'inscription ?";
+$lang['regprotect'] = "Protéger également le formulaire d'inscription ?";
 $lang['forusers']   = "Utiliser également le CAPTCHA pour les utilisateurs connectés ?";
 $lang['width']      = "Largeur du CAPTCHA image (en pixels)";
 $lang['height']     = "Hauteur du CAPTCHA image (en pixels)";
