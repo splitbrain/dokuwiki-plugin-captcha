fa42f293a5469c4134696016f23f05ba2e53bece
Danny Lin <danny0838@pchome.com.tw>
2010-12-21 21:19:48 +0100

    Traditional Chinese translation added

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Traditional Chinese language file
+ *
+ * @author Danny Lin <danny0838@pchome.com.tw>
+ */
+
+$lang['testfailed']  = "很抱歉，您沒有輸入正確的 CAPTCHA 驗證碼。";
+$lang['fillcaptcha'] = "請將字母填入方框。";
+$lang['soundlink']   = "如果您無法閱讀圖片中的字母，請下載收聽這個 WAV 檔。";
---
+++ lang/zh-tw/settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Traditional Chinese language file
+ *
+ * @author Danny Lin <danny0838@pchome.com.tw>
+ */
+
+$lang['mode']          = "使用哪種 CAPTCHA 類型？";
+$lang['mode_o_js']     = "文字 (預先用 Javascript 填入)";
+$lang['mode_o_text']   = "文字 (手動填入)";
+$lang['mode_o_image']  = "圖片 (易用性差)";
+$lang['mode_o_audio']  = "圖片+聲音 (易用性較佳)";
+$lang['mode_o_figlet'] = "Figlet ASCII 藝術字 (易用性差)";
+
+$lang['regprotect'] = "保護註冊表單嗎？";
+$lang['forusers']   = "已登入使用者也要 CAPTCHA 驗證嗎？";
+$lang['width']      = "CAPTCHA 圖片寬度 (像素)";
+$lang['height']     = "CAPTCHA 圖片高度 (像素)";
+
