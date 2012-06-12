677d92ef914247fd703c8a33c102b5f0e8883e9e
Dryoo <dryoo@live.com>
2012-06-12 20:46:14 +0200

    Korean translation

---
+++ lang/ko/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * English language file
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['testfailed']  = "스팸방지용 문자가 정확하지 않습니다. 다시 입력해보세요.";
+$lang['fillcaptcha'] = "변경한 내용을 저장하려면 우측에 보이는 스팸방지용 문자를 입력하세요.";
+$lang['soundlink']   = "글씨를 읽을 수 없다면 파일을 받아 소리를 들어보세요.";
+
+
---
+++ lang/ko/settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * English language file
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['mode']          = "어떤 방식의 CAPTCHA를 쓸가요?";
+$lang['mode_o_js']     = "문자 (prefilled with JavaScript)";
+$lang['mode_o_text']   = "문자 (manual only)";
+$lang['mode_o_image']  = "그림 (bad accessibility)";
+$lang['mode_o_audio']  = "그림+소리 (better accessibility)";
+$lang['mode_o_figlet'] = "Figlet 문자 그림 (bad accessibility)";
+
+$lang['regprotect'] = "가입시에도 CAPTCHA를 사용합니까?";
+$lang['forusers']   = "로긴한 사용자도 CAPTCHA를 사용합니까?";
+$lang['width']      = "CAPTCHA 그림의 넓이 (pixel)";
+$lang['height']     = "CAPTCHA 그림의 높이 (pixel)";
+
