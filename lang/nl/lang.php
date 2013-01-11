3c08380114041af36d55cc19bdb983ed6743c1d4
Ruben Schouten <ruben.schouten@me.com>
2013-01-11 14:47:08 +0100

    Dutch translation

---
+++ lang/nl/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Dutch language file
+ *
+ * @author Ruben Schouten <mail@ruben.cc>
+ */
+
+$lang['testfailed']  = "Sorry, maar de CAPTCHA is onjuist beantwoord. Misschien ben je toch geen mens?";
+$lang['fillcaptcha'] = "Tik de letters in het onderstaande vakje over om aan te tonen dat je een mens bent.";
+$lang['soundlink']   = "Als je de letters in de afbeelding niet kunt lezen kun je dit .wav bestand downloaden om ze te laten voorlezen.";
+
---
+++ lang/nl/settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Dutch language file
+ *
+ * @author Ruben Schouten <mail@ruben.cc>
+ */
+
+$lang['mode']          = "Welk type CAPTCHA wil je gebruiken?";
+$lang['mode_o_js']     = "Tekst (automatisch ingevuld via JavaScript)";
+$lang['mode_o_text']   = "Tekst (handmatig overtikken)";
+$lang['mode_o_image']  = "Afbeelding (slechte toegankelijkhied)";
+$lang['mode_o_audio']  = "Afbeelding+Audio (betere toegankelijkheid)";
+$lang['mode_o_figlet'] = "Figlet ASCII Art (slechte toegankelijkheid)";
+
+$lang['regprotect'] = "Het aanmeldformulier ook beschermen?";
+$lang['forusers']   = "Ook CAPTCHA voor ingelogde gebruikers gebruiken?";
+$lang['width']      = "Breedte van de CAPTCHA afbeelding (pixels)";
+$lang['height']     = "Hoogte van de CAPTCHA afbeelding (pixels)";
+
