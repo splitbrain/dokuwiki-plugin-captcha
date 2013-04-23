633ea6840449afc3b1fb57a1fa49cf48c7bd5b09
qinghao <qingxianhao@gmail.com>
2013-04-23 22:17:09 +0800

    translate Math problem Chinese strings

--- lang/zh/settings.php
+++ lang/zh/settings.php
@@ -8,6 +8,7 @@
 $lang['mode']          = "使用什么类型的验证码？";
 $lang['mode_o_js']     = "文本 (预先由 JavaScript 填写)";
 $lang['mode_o_text']   = "文本 (手动输入)";
+$lang['mode_o_math']   = "算术题";
 $lang['mode_o_image']  = "图片 (无障碍性差)";
 $lang['mode_o_audio']  = "图片+音频 (更好的无障碍性)";
 $lang['mode_o_figlet'] = "Figlet ASCII 艺术 (无障碍性差)";
