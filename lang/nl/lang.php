5676b4ece4f86ea08b8ba4ba32bf154309a18c4a
Mark Prins <mprins@users.sf.net>
2013-04-29 16:35:08 +0300

    Update lang.php
    add ```fillmath``` key/value

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -7,5 +7,6 @@
 
 $lang['testfailed']  = "Sorry, maar de CAPTCHA is onjuist beantwoord. Misschien ben je toch geen mens?";
 $lang['fillcaptcha'] = "Tik de letters in het onderstaande vakje over om aan te tonen dat je een mens bent.";
+$lang['fillmath']    = "Geef antwoord op de som om aan te tonen dat je een mens bent.";
 $lang['soundlink']   = "Als je de letters in de afbeelding niet kunt lezen kun je dit .wav bestand downloaden om ze te laten voorlezen.";
 
