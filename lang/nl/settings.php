6610d46e0a150694b957bd3b218236d079ab9190
Mark Prins <mprins@users.sf.net>
2013-04-29 16:40:21 +0300

    Update settings.php
    add ```mode_o_math``` key/value

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -3,11 +3,13 @@
  * Dutch language file
  *
  * @author Ruben Schouten <mail@ruben.cc>
+ * @author Mark C. Prins <mprins@users.sf.net>
  */
 
 $lang['mode']          = "Welk type CAPTCHA wil je gebruiken?";
 $lang['mode_o_js']     = "Tekst (automatisch ingevuld via JavaScript)";
 $lang['mode_o_text']   = "Tekst (handmatig overtikken)";
+$lang['mode_o_math']   = "Wiskunde opgave (eenvoudige rekensom)";
 $lang['mode_o_image']  = "Afbeelding (slechte toegankelijkhied)";
 $lang['mode_o_audio']  = "Afbeelding+Audio (betere toegankelijkheid)";
 $lang['mode_o_figlet'] = "Figlet ASCII Art (slechte toegankelijkheid)";
@@ -16,4 +18,3 @@ $lang['regprotect'] = "Het aanmeldformulier ook beschermen?";
 $lang['forusers']   = "Ook CAPTCHA voor ingelogde gebruikers gebruiken?";
 $lang['width']      = "Breedte van de CAPTCHA afbeelding (pixels)";
 $lang['height']     = "Hoogte van de CAPTCHA afbeelding (pixels)";
-
