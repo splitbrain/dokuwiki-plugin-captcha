53d98bb71cc6f24a1be99b1a3b2c93abb72775ec
Mark Prins <mprins@users.sf.net>
2013-04-29 16:38:39 +0300

    Update lang.php

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -3,10 +3,11 @@
  * Dutch language file
  *
  * @author Ruben Schouten <mail@ruben.cc>
+ * @author Mark C. Prins <mprins@users.sf.net>
  */
 
 $lang['testfailed']  = "Sorry, maar de CAPTCHA is onjuist beantwoord. Misschien ben je toch geen mens?";
 $lang['fillcaptcha'] = "Tik de letters in het onderstaande vakje over om aan te tonen dat je een mens bent.";
-$lang['fillmath']    = "Geef antwoord op de som om aan te tonen dat je een mens bent.";
+$lang['fillmath']    = "Geef antwoord op de rekensom om aan te tonen dat je een mens bent.";
 $lang['soundlink']   = "Als je de letters in de afbeelding niet kunt lezen kun je dit .wav bestand downloaden om ze te laten voorlezen.";
 
