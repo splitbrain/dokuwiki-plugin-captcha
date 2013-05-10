104ec26843094478ee39495271dc0fadce69ca23
Andreas Gohr <andi@splitbrain.org>
2013-05-10 17:10:01 +0200

    some cosmetic code cleanup

--- helper.php
+++ helper.php
@@ -5,31 +5,30 @@
  */
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
 require_once(DOKU_INC.'inc/blowfish.php');
 
 class helper_plugin_captcha extends DokuWiki_Plugin {
 
-    protected $field_in  = 'plugin__captcha';
+    protected $field_in = 'plugin__captcha';
     protected $field_sec = 'plugin__captcha_secret';
-    protected $field_hp  = 'plugin__captcha_honeypot';
+    protected $field_hp = 'plugin__captcha_honeypot';
 
     /**
      * Constructor. Initializes field names
      */
-    function __construct(){
-        $this->field_in  = md5($this->_fixedIdent() . $this->field_in);
-        $this->field_sec = md5($this->_fixedIdent() . $this->field_sec);
-        $this->field_hp  = md5($this->_fixedIdent() . $this->field_hp);
+    public function __construct() {
+        $this->field_in  = md5($this->_fixedIdent().$this->field_in);
+        $this->field_sec = md5($this->_fixedIdent().$this->field_sec);
+        $this->field_hp  = md5($this->_fixedIdent().$this->field_hp);
     }
-
 
     /**
      * Check if the CAPTCHA should be used. Always check this before using the methods below.
      *
      * @return bool true when the CAPTCHA should be used
      */
-    function isEnabled(){
+    public function isEnabled() {
         if(!$this->getConf('forusers') && $_SERVER['REMOTE_USER']) return false;
         return true;
     }
@@ -37,26 +36,26 @@ class helper_plugin_captcha extends DokuWiki_Plugin {
     /**
      * Returns the HTML to display the CAPTCHA with the chosen method
      */
-    function getHTML(){
+    public function getHTML() {
         global $ID;
 
-        $rand = (float) (rand(0,10000))/10000;
-        if($this->getConf('mode') == 'math'){
-            $code = $this->_generateMATH($this->_fixedIdent(),$rand);
+        $rand = (float) (rand(0, 10000)) / 10000;
+        if($this->getConf('mode') == 'math') {
+            $code = $this->_generateMATH($this->_fixedIdent(), $rand);
             $code = $code[0];
             $text = $this->getLang('fillmath');
         } else {
-            $code = $this->_generateCAPTCHA($this->_fixedIdent(),$rand);
+            $code = $this->_generateCAPTCHA($this->_fixedIdent(), $rand);
             $text = $this->getLang('fillcaptcha');
         }
-        $secret = PMA_blowfish_encrypt($rand,auth_cookiesalt());
+        $secret = PMA_blowfish_encrypt($rand, auth_cookiesalt());
 
-        $out  = '';
+        $out = '';
         $out .= '<div id="plugin__captcha_wrapper">';
         $out .= '<input type="hidden" name="'.$this->field_sec.'" value="'.hsc($secret).'" />';
         $out .= '<label for="plugin__captcha">'.$text.'</label> ';
 
-        switch($this->getConf('mode')){
+        switch($this->getConf('mode')) {
             case 'text':
             case 'math':
                 $out .= $code;
@@ -66,25 +65,25 @@ class helper_plugin_captcha extends DokuWiki_Plugin {
                 break;
             case 'image':
                 $out .= '<img src="'.DOKU_BASE.'lib/plugins/captcha/img.php?secret='.rawurlencode($secret).'&amp;id='.$ID.'" '.
-                        ' width="'.$this->getConf('width').'" height="'.$this->getConf('height').'" alt="" /> ';
+                    ' width="'.$this->getConf('width').'" height="'.$this->getConf('height').'" alt="" /> ';
                 break;
             case 'audio':
                 $out .= '<img src="'.DOKU_BASE.'lib/plugins/captcha/img.php?secret='.rawurlencode($secret).'&amp;id='.$ID.'" '.
-                        ' width="'.$this->getConf('width').'" height="'.$this->getConf('height').'" alt="" /> ';
+                    ' width="'.$this->getConf('width').'" height="'.$this->getConf('height').'" alt="" /> ';
                 $out .= '<a href="'.DOKU_BASE.'lib/plugins/captcha/wav.php?secret='.rawurlencode($secret).'&amp;id='.$ID.'"'.
-                        ' class="JSnocheck" title="'.$this->getLang('soundlink').'">';
+                    ' class="JSnocheck" title="'.$this->getLang('soundlink').'">';
                 $out .= '<img src="'.DOKU_BASE.'lib/plugins/captcha/sound.png" width="16" height="16"'.
-                        ' alt="'.$this->getLang('soundlink').'" /></a>';
+                    ' alt="'.$this->getLang('soundlink').'" /></a>';
                 break;
             case 'figlet':
                 require_once(dirname(__FILE__).'/figlet.php');
                 $figlet = new phpFiglet();
-                if($figlet->loadfont(dirname(__FILE__).'/figlet.flf')){
+                if($figlet->loadfont(dirname(__FILE__).'/figlet.flf')) {
                     $out .= '<pre>';
                     $out .= rtrim($figlet->fetch($code));
                     $out .= '</pre>';
-                }else{
-                    msg('Failed to load figlet.flf font file. CAPTCHA broken',-1);
+                } else {
+                    msg('Failed to load figlet.flf font file. CAPTCHA broken', -1);
                 }
                 break;
         }
@@ -102,23 +101,23 @@ class helper_plugin_captcha extends DokuWiki_Plugin {
      * @param  bool $msg when true, an error will be signalled through the msg() method
      * @return bool true when the answer was correct, otherwise false
      */
-    function check($msg=true){
+    public function check($msg = true) {
         // compare provided string with decrypted captcha
-        $rand = PMA_blowfish_decrypt($_REQUEST[$this->field_sec],auth_cookiesalt());
+        $rand = PMA_blowfish_decrypt($_REQUEST[$this->field_sec], auth_cookiesalt());
 
-        if($this->getConf('mode') == 'math'){
-            $code = $this->_generateMATH($this->_fixedIdent(),$rand);
+        if($this->getConf('mode') == 'math') {
+            $code = $this->_generateMATH($this->_fixedIdent(), $rand);
             $code = $code[1];
-        }else{
-            $code = $this->_generateCAPTCHA($this->_fixedIdent(),$rand);
+        } else {
+            $code = $this->_generateCAPTCHA($this->_fixedIdent(), $rand);
         }
 
         if(!$_REQUEST[$this->field_sec] ||
-           !$_REQUEST[$this->field_in] ||
-           strtoupper($_REQUEST[$this->field_in]) != $code ||
-           trim($_REQUEST[$this->field_hp]) !== ''
-          ){
-            if($msg) msg($this->getLang('testfailed'),-1);
+            !$_REQUEST[$this->field_in] ||
+            strtoupper($_REQUEST[$this->field_in]) != $code ||
+            trim($_REQUEST[$this->field_hp]) !== ''
+        ) {
+            if($msg) msg($this->getLang('testfailed'), -1);
             return false;
         }
         return true;
@@ -132,31 +131,33 @@ class helper_plugin_captcha extends DokuWiki_Plugin {
      * after midnight will result in a failed CAPTCHA once, but makes sure it can
      * not be reused which is especially important for the registration form where the
      * $ID usually won't change.
+     *
+     * @return string
      */
-    function _fixedIdent(){
+    public function _fixedIdent() {
         global $ID;
         $lm = @filemtime(wikiFN($ID));
         $td = date('Y-m-d');
         return auth_browseruid().
-               auth_cookiesalt().
-               $ID.$lm.$td;
+            auth_cookiesalt().
+            $ID.$lm.$td;
     }
 
     /**
      * Generates a random 5 char string
      *
-     * @param $fixed string - the fixed part, any string
-     * @param $rand  float  - some random number between 0 and 1
+     * @param $fixed string the fixed part, any string
+     * @param $rand  float  some random number between 0 and 1
      * @return string
      */
-    function _generateCAPTCHA($fixed,$rand){
-        $fixed = hexdec(substr(md5($fixed),5,5)); // use part of the md5 to generate an int
+    public function _generateCAPTCHA($fixed, $rand) {
+        $fixed   = hexdec(substr(md5($fixed), 5, 5)); // use part of the md5 to generate an int
         $numbers = md5($rand * $fixed); // combine both values
 
         // now create the letters
         $code = '';
-        for($i=0;$i<10;$i+=2){
-            $code .= chr(floor(hexdec($numbers[$i].$numbers[$i+1])/10) + 65);
+        for($i = 0; $i < 10; $i += 2) {
+            $code .= chr(floor(hexdec($numbers[$i].$numbers[$i + 1]) / 10) + 65);
         }
 
         return $code;
@@ -165,16 +166,16 @@ class helper_plugin_captcha extends DokuWiki_Plugin {
     /**
      * Create a mathematical task and its result
      *
-     * @param $fixed string - the fixed part, any string
-     * @param $rand  float  - some random number between 0 and 1
-     * @return string
+     * @param $fixed string the fixed part, any string
+     * @param $rand  float  some random number between 0 and 1
+     * @return array taks, result
      */
-    function _generateMATH($fixed, $rand){
-        $fixed = hexdec(substr(md5($fixed),5,5)); // use part of the md5 to generate an int
+    protected function _generateMATH($fixed, $rand) {
+        $fixed   = hexdec(substr(md5($fixed), 5, 5)); // use part of the md5 to generate an int
         $numbers = md5($rand * $fixed); // combine both values
 
         // first letter is the operator (+/-)
-        $op  = (hexdec($numbers[0]) > 8 ) ? -1 : 1;
+        $op  = (hexdec($numbers[0]) > 8) ? -1 : 1;
         $num = array(hexdec($numbers[1].$numbers[2]), hexdec($numbers[3]));
 
         // we only want positive results
@@ -182,15 +183,17 @@ class helper_plugin_captcha extends DokuWiki_Plugin {
 
         // prepare result and task text
         $res  = $num[0] + ($num[1] * $op);
-        $task = $num[0] . (($op < 0) ? '&nbsp;-&nbsp;' : '&nbsp;+&nbsp;') . $num[1] . '&nbsp;=&nbsp;?';
+        $task = $num[0].(($op < 0) ? '&nbsp;-&nbsp;' : '&nbsp;+&nbsp;').$num[1].'&nbsp;=&nbsp;?';
 
         return array($task, $res);
     }
 
     /**
      * Create a CAPTCHA image
+     *
+     * @param string $text the letters to display
      */
-    function _imageCAPTCHA($text){
+    public function _imageCAPTCHA($text) {
         $w = $this->getConf('width');
         $h = $this->getConf('height');
 
@@ -201,21 +204,21 @@ class helper_plugin_captcha extends DokuWiki_Plugin {
         imagecolorallocate($img, 255, 255, 255);
 
         // add some lines as background noise
-        for ($i = 0; $i < 30; $i++) {
-            $color = imagecolorallocate($img,rand(100, 250),rand(100, 250),rand(100, 250));
-            imageline($img,rand(0,$w),rand(0,$h),rand(0,$w),rand(0,$h),$color);
+        for($i = 0; $i < 30; $i++) {
+            $color = imagecolorallocate($img, rand(100, 250), rand(100, 250), rand(100, 250));
+            imageline($img, rand(0, $w), rand(0, $h), rand(0, $w), rand(0, $h), $color);
         }
 
         // draw the letters
-        for ($i = 0; $i < strlen($text); $i++){
+        for($i = 0; $i < strlen($text); $i++) {
             $font  = $fonts[array_rand($fonts)];
             $color = imagecolorallocate($img, rand(0, 100), rand(0, 100), rand(0, 100));
-            $size  = rand(floor($h/1.8),floor($h*0.7));
+            $size  = rand(floor($h / 1.8), floor($h * 0.7));
             $angle = rand(-35, 35);
 
-            $x = ($w*0.05) +  $i * floor($w*0.9/5);
-            $cheight = $size + ($size*0.5);
-            $y = floor($h / 2 + $cheight / 3.8);
+            $x       = ($w * 0.05) + $i * floor($w * 0.9 / 5);
+            $cheight = $size + ($size * 0.5);
+            $y       = floor($h / 2 + $cheight / 3.8);
 
             imagettftext($img, $size, $angle, $x, $y, $color, $font, $text[$i]);
         }
@@ -224,6 +227,5 @@ class helper_plugin_captcha extends DokuWiki_Plugin {
         imagepng($img);
         imagedestroy($img);
     }
-
 
 }
--- img.php
+++ img.php
@@ -6,16 +6,17 @@
  * @author     Andreas Gohr <gohr@cosmocode.de>
  */
 
-if(!defined('DOKU_INC')) define('DOKU_INC',dirname(__FILE__).'/../../../');
-define('NOSESSION',true);
+if(!defined('DOKU_INC')) define('DOKU_INC', dirname(__FILE__).'/../../../');
+define('NOSESSION', true);
 define('DOKU_DISABLE_GZIP_OUTPUT', 1);
 require_once(DOKU_INC.'inc/init.php');
 require_once(DOKU_INC.'inc/auth.php');
 
-$ID = $_REQUEST['id'];
-$plugin = plugin_load('helper','captcha');
-$rand = PMA_blowfish_decrypt($_REQUEST['secret'],auth_cookiesalt());
-$code = $plugin->_generateCAPTCHA($plugin->_fixedIdent(),$rand);
+$ID     = $_REQUEST['id'];
+/** @var helper_plugin_captcha $plugin */
+$plugin = plugin_load('helper', 'captcha');
+$rand   = PMA_blowfish_decrypt($_REQUEST['secret'], auth_cookiesalt());
+$code   = $plugin->_generateCAPTCHA($plugin->_fixedIdent(), $rand);
 $plugin->_imageCAPTCHA($code);
 
 //Setup VIM: ex: et ts=4 enc=utf-8 :
