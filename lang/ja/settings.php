23a509531e8ddc2cd62e8ccbf491a7d388dd7cf2
Hideaki SAWADA <chuno@live.jp>
2013-09-20 13:55:36 +0200

    translation update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -1,20 +1,23 @@
 <?php
+
 /**
- * Japanese language file
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author OHTSU Yoshifumi <dev@decomo.info>
+ * @author Hideaki SAWADA <chuno@live.jp>
  */
-
-$lang['mode']          = "認証の方式";
-$lang['mode_o_js']     = "文字 (JavaScriptによる自動入力)";
-$lang['mode_o_text']   = "文字 (手動入力)";
-$lang['mode_o_math']   = "計算式";
-$lang['mode_o_image']  = "画像 (低アクセシビリティ)";
-$lang['mode_o_audio']  = "画像+音声 (中アクセシビリティ)";
-$lang['mode_o_figlet'] = "Figlet [アルファベットAA] (低アクセシビリティ)";
-
-$lang['regprotect'] = "ユーザー登録時にCAPTCHA認証を行う";
-$lang['forusers']   = "ログインユーザーに対してもCAPTCHA認証を行う";
-$lang['width']      = "CAPTCHA画像の幅　(ピクセル)";
-$lang['height']     = "CAPTCHA画像の高さ(ピクセル)";
-
+$lang['mode']                  = '認証の方式';
+$lang['mode_o_js']             = '文字 (JavaScriptによる自動入力)';
+$lang['mode_o_text']           = '文字 (手動入力)';
+$lang['mode_o_math']           = '計算式';
+$lang['mode_o_question']       = '固定質問';
+$lang['mode_o_image']          = '画像 (低アクセシビリティ)';
+$lang['mode_o_audio']          = '画像+音声 (中アクセシビリティ)';
+$lang['mode_o_figlet']         = 'Figlet [アルファベットAA] (低アクセシビリティ)';
+$lang['regprotect']            = 'ユーザー登録時にCAPTCHA認証を行う';
+$lang['forusers']              = 'ログインユーザーに対してもCAPTCHA認証を行う';
+$lang['lettercount']           = '使用する文字数（3～16）。文字数を増やす場合は下の画像の幅も同様に増やして下さい。';
+$lang['width']                 = 'CAPTCHA画像の幅　(ピクセル)';
+$lang['height']                = 'CAPTCHA画像の高さ(ピクセル)';
+$lang['question']              = '固定質問方式の質問';
+$lang['answer']                = '固定質問方式の回答';
