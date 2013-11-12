12c40544b8b902e515eafa161a26d9a7cb747bda
soer9648 <soer9648@eucl.dk>
2013-11-12 20:54:34 +0100

    translation update

---
+++ lang/da/settings.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author soer9648 <soer9648@eucl.dk>
+ */
+$lang['mode']                  = 'Hvilken type CAPTCHA skal benyttes?';
+$lang['mode_o_js']             = 'Tekst (præudfyldt af JavaScript)';
+$lang['mode_o_text']           = 'Tekst (kun manuelt)';
+$lang['mode_o_math']           = 'Matematikproblem';
+$lang['mode_o_question']       = 'Løsning';
+$lang['mode_o_image']          = 'Billede (dårlig tilgængelighed)';
+$lang['mode_o_audio']          = 'Billede+Audio (bedre tilgængelighed)';
+$lang['mode_o_figlet']         = 'Figlet ASCII Art (dårlig tilgængelighed)';
+$lang['regprotect']            = 'Beskyt også registreringsformularen?';
+$lang['forusers']              = 'Benyt også CAPTCHA til brugere der er logget ind?';
+$lang['lettercount']           = 'Antal af bogstaver der skal benyttes (3-16). Hvis du øger antallet, skal du også huske at øge bredden af billedet herunder.';
+$lang['width']                 = 'Bredden af CAPTCHA-billedet (pixel)';
+$lang['height']                = 'Højden af CAPTCHA-billedet (pixel)';
+$lang['question']              = 'Spørgsmål til fast-spørgsmål-tilstand';
+$lang['answer']                = 'Svar til fast-spørgsmål-tilstand';
