d787956ca69cdc0983afc30c2ebe40335c5e7fa2
bruno <bruno@ninesys.fr>
2013-12-12 23:15:05 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,11 +1,13 @@
 <?php
-/**
- * French language file
- *
- * @author Emmanuel Dupin <seedfloyd@gmail.com>
- */
 
-$lang['testfailed']  = "Désolé, vous n'avez pas répondu correctement au test anti-spam.";
-$lang['fillcaptcha'] = "Merci de recopier le code ci-contre (anti-spam) :";
-$lang['soundlink']   = "Si vous ne pouvez déchiffrer le code, téléchargez ce fichier .wav pour l'entendre.";
-$lang['honeypot']    = "Merci de laisser ce champ vide : ";
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Emmanuel Dupin <seedfloyd@gmail.com>
+ * @author bruno <bruno@ninesys.fr>
+ */
+$lang['testfailed']            = 'Désolé, vous n\'avez pas répondu correctement au test anti-spam.';
+$lang['fillcaptcha']           = 'Merci de recopier le code ci-contre (anti-spam) :';
+$lang['fillmath']              = 'S\'il vous plaît résolvez l\'équation suivante pour prouver que vous êtes humain.';
+$lang['soundlink']             = 'Si vous ne pouvez déchiffrer le code, téléchargez ce fichier .wav pour l\'entendre.';
+$lang['honeypot']              = 'Merci de laisser ce champ vide : ';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,19 +1,22 @@
 <?php
+
 /**
- * French language file
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
+ * @author bruno <bruno@ninesys.fr>
  */
-
-$lang['mode']          = "Quel type de CAPTCHA utiliser ?";
-$lang['mode_o_js']     = "Texte (prérempli avec JavaScript)";
-$lang['mode_o_text']   = "Texte (remplissage manuel)";
-$lang['mode_o_image']  = "Image (peu accessible)";
-$lang['mode_o_audio']  = "Image + Audio (meilleure accessibilité)";
-$lang['mode_o_figlet'] = "ASCII Art (peu accessible)";
-
-$lang['regprotect'] = "Protéger également le formulaire d'inscription ?";
-$lang['forusers']   = "Utiliser également le CAPTCHA pour les utilisateurs connectés ?";
-$lang['width']      = "Largeur du CAPTCHA image (en pixels)";
-$lang['height']     = "Hauteur du CAPTCHA image (en pixels)";
-
+$lang['mode']                  = 'Quel type de CAPTCHA utiliser ?';
+$lang['mode_o_js']             = 'Texte (prérempli avec JavaScript)';
+$lang['mode_o_text']           = 'Texte (remplissage manuel)';
+$lang['mode_o_math']           = 'Problème mathématique';
+$lang['mode_o_question']       = 'Question fixe';
+$lang['mode_o_image']          = 'Image (peu accessible)';
+$lang['mode_o_audio']          = 'Image + Audio (meilleure accessibilité)';
+$lang['mode_o_figlet']         = 'ASCII Art (peu accessible)';
+$lang['regprotect']            = 'Protéger également le formulaire d\'inscription ?';
+$lang['forusers']              = 'Utiliser également le CAPTCHA pour les utilisateurs connectés ?';
+$lang['width']                 = 'Largeur du CAPTCHA image (en pixels)';
+$lang['height']                = 'Hauteur du CAPTCHA image (en pixels)';
+$lang['question']              = 'Question pour le mode question fixe';
+$lang['answer']                = 'Réponse pour le mode question fixe';
