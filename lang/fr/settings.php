03b4f17904c276a3e78ffde9afc0ff88d850257f
Nicolas Friedli <nicolas@theologique.ch>
2013-12-12 16:00:17 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,19 +1,23 @@
 <?php
+
 /**
- * French language file
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
+ * @author Nicolas Friedli <nicolas@theologique.ch>
  */
-
-$lang['mode']          = "Quel type de CAPTCHA utiliser ?";
-$lang['mode_o_js']     = "Texte (prérempli avec JavaScript)";
-$lang['mode_o_text']   = "Texte (remplissage manuel)";
-$lang['mode_o_image']  = "Image (peu accessible)";
-$lang['mode_o_audio']  = "Image + Audio (meilleure accessibilité)";
-$lang['mode_o_figlet'] = "ASCII Art (peu accessible)";
-
-$lang['regprotect'] = "Protéger également le formulaire d'inscription ?";
-$lang['forusers']   = "Utiliser également le CAPTCHA pour les utilisateurs connectés ?";
-$lang['width']      = "Largeur du CAPTCHA image (en pixels)";
-$lang['height']     = "Hauteur du CAPTCHA image (en pixels)";
-
+$lang['mode']                  = 'Quel type de CAPTCHA utiliser ?';
+$lang['mode_o_js']             = 'Texte (prérempli avec JavaScript)';
+$lang['mode_o_text']           = 'Texte (remplissage manuel)';
+$lang['mode_o_math']           = 'Problème de math';
+$lang['mode_o_question']       = 'Question choisie';
+$lang['mode_o_image']          = 'Image (peu accessible)';
+$lang['mode_o_audio']          = 'Image + Audio (meilleure accessibilité)';
+$lang['mode_o_figlet']         = 'ASCII Art (peu accessible)';
+$lang['regprotect']            = 'Protéger également le formulaire d\'inscription ?';
+$lang['forusers']              = 'Utiliser également le CAPTCHA pour les utilisateurs connectés ?';
+$lang['lettercount']           = 'Nombre de caractères à utiliser (3-16). Si vous augmentez le nombre, soyez certain que la largeur de l\'image est suffisante.';
+$lang['width']                 = 'Largeur du CAPTCHA image (en pixels)';
+$lang['height']                = 'Hauteur du CAPTCHA image (en pixels)';
+$lang['question']              = 'Question pour le mode "question choisir"';
+$lang['answer']                = 'Réponse pour le monde "question choisie"';
