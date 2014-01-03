bd051478d523a7364ddf0311ee0d9c318541eba6
Fabrice Dejaigher <fabrice@chtiland.com>
2014-01-03 11:55:06 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -5,9 +5,10 @@
  * 
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
  * @author bruno <bruno@ninesys.fr>
+ * @author Fabrice Dejaigher <fabrice@chtiland.com>
  */
-$lang['testfailed']            = 'Désolé, vous n\'avez pas répondu correctement au test anti-spam.';
-$lang['fillcaptcha']           = 'Merci de recopier le code ci-contre (anti-spam) :';
+$lang['testfailed']            = 'Désolé, vous n\'avez pas répondu correctement au test anti-spam. Peut-être n\'êtes vous pas humain ?';
+$lang['fillcaptcha']           = 'Merci de recopier le code ci-contre pour prouver que vous êtes humain :';
 $lang['fillmath']              = 'S\'il vous plaît résolvez l\'équation suivante pour prouver que vous êtes humain.';
-$lang['soundlink']             = 'Si vous ne pouvez déchiffrer le code, téléchargez ce fichier .wav pour l\'entendre.';
+$lang['soundlink']             = 'Si vous ne pouvez pas lire le code, téléchargez ce fichier .wav pour l\'écouter.';
 $lang['honeypot']              = 'Merci de laisser ce champ vide : ';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -5,18 +5,20 @@
  * 
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
  * @author bruno <bruno@ninesys.fr>
+ * @author Fabrice Dejaigher <fabrice@chtiland.com>
  */
 $lang['mode']                  = 'Quel type de CAPTCHA utiliser ?';
 $lang['mode_o_js']             = 'Texte (prérempli avec JavaScript)';
 $lang['mode_o_text']           = 'Texte (remplissage manuel)';
 $lang['mode_o_math']           = 'Problème mathématique';
 $lang['mode_o_question']       = 'Question fixe';
-$lang['mode_o_image']          = 'Image (peu accessible)';
+$lang['mode_o_image']          = 'Image (mauvaise accessibilité)';
 $lang['mode_o_audio']          = 'Image + Audio (meilleure accessibilité)';
-$lang['mode_o_figlet']         = 'ASCII Art (peu accessible)';
+$lang['mode_o_figlet']         = 'ASCII Art (mauvaise accessibilité)';
 $lang['regprotect']            = 'Protéger également le formulaire d\'inscription ?';
 $lang['forusers']              = 'Utiliser également le CAPTCHA pour les utilisateurs connectés ?';
-$lang['width']                 = 'Largeur du CAPTCHA image (en pixels)';
-$lang['height']                = 'Hauteur du CAPTCHA image (en pixels)';
-$lang['question']              = 'Question pour le mode question fixe';
-$lang['answer']                = 'Réponse pour le mode question fixe';
+$lang['lettercount']           = 'Nombre de lettres à utiliser (3 à 16). Pensez à augmenter la taille de l\'image ci-dessous en adéquation avec le nombre de lettres afin que celles-ci soient correctement affichées.';
+$lang['width']                 = 'Largeur de l\'image du CAPTCHA (en pixels)';
+$lang['height']                = 'Hauteur de l\'image du CAPTCHA (en pixels)';
+$lang['question']              = 'Question pour le mode \'question fixe\'';
+$lang['answer']                = 'Réponse pour le mode \'question fixe\'';
