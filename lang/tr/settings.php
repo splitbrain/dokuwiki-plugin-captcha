7c9b45a14d95dc92d3a22a90bb77190ec13627f3
Ehliman <ehlimansen@gmail.com>
2014-05-02 08:10:12 +0200

    translation update

---
+++ lang/tr/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Ehliman <ehlimansen@gmail.com>
+ */
+$lang['mode_o_math']           = 'Matematik Problemi';
+$lang['mode_o_question']       = 'Sabit Soru';
+$lang['mode_o_image']          = 'Resim (Kötü erişebilirlik)';
