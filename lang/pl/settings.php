9a9144b8ee61faefe1c9d8016e4a1f73d2fafc15
Mati <mackosa@wp.pl>
2014-06-04 22:25:06 +0200

    translation update

--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -4,12 +4,15 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Paweł Jan Czochański <czochanski@gmail.com>
+ * @author Mati <mackosa@wp.pl>
  */
 $lang['mode']                  = 'Jaki typ CAPTCHA zastosować?';
+$lang['mode_o_text']           = 'Tekst (tylko ręcznie)';
 $lang['mode_o_math']           = 'Problem matematyczny';
 $lang['mode_o_question']       = 'Stałe pytanie';
 $lang['mode_o_image']          = 'Obraz (słaba dostępność)';
 $lang['mode_o_audio']          = 'Obraz+Dźwięk (lepsza dostępność)';
+$lang['mode_o_figlet']         = 'Sztuka figletowych ASCII (słaba dostępność)';
 $lang['regprotect']            = 'Ochraniać również proces rejestracji?';
 $lang['forusers']              = 'Stosować CAPTCHA również dla zalogowanych użytkowników?';
 $lang['lettercount']           = 'Wykorzystywane liczby i litery (3-16). Pamiętaj by wraz ze wzrostem ich ilości zwiększać również szerokość obrazu poniżej.';
