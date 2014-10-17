e110410bdca243b332931ddb1ebc407a08ce8122
chobkwon <chobkwon@gmail.com>
2014-10-17 14:05:06 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -5,8 +5,9 @@
  * 
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author Myeongjin <aranet100@gmail.com>
+ * @author chobkwon <chobkwon@gmail.com>
  */
-$lang['testfailed']            = '죄송하지만 CAPTCHA(캡차)가 올바르지 않습니다. 아마도 전혀 인간이 아니죠?';
+$lang['testfailed']            = '죄송하지만 CAPTCHA(캡차)가 올바르지 않습니다. 아마도 인간이 아니죠?';
 $lang['fillcaptcha']           = '인간임을 증명하기 위해 상자에 있는 모든 글자를 채워주세요.';
 $lang['fillmath']              = '인간임을 증명하기 위해 다음 방정식을 푸세요.';
 $lang['soundlink']             = '그림에 있는 글자를 읽을 수 없다면, 당신에게 들려줄 이 .wav 파일을 다운로드하세요.';
