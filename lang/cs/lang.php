eb7d8e3ad8394da87684e2316c9fe9032eac3b74
Mišo <miso@fykos.cz>
2015-02-06 14:13:31 +0100

    czech translation update

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -8,4 +8,5 @@
 $lang['testfailed']  = "Bohužel, ale na CAPTCHA nebylo odpovězeno správně. Jste vůbec člověk?";
 $lang['fillcaptcha'] = "Vyplňte, prosím, všechna písmena v poli, abyste dokázali, že nejste robot.";
 $lang['soundlink']   = "Pokud nedokážete přečíst písmena na obrázku, stáhněte si tento .wav soubor, kde je text přečtený.";
+$lang['fillmath']    = "Prosíme, vyřešte nasledující rovnici, abyste dokázali, že nejste robot.";
 
