a6286db881b2c14ac0384dfb687810a4fc32bed6
Andreas Gohr <andi@splitbrain.org>
2015-04-17 22:34:19 +0200

    added HTML5 audio player for the audio version

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-/**
- * Autofill and hide the whole captcha stuff in the simple JS mode
- */
+
 jQuery(function () {
+    var $wrap = jQuery('#plugin__captcha_wrapper');
+    if(!$wrap.length) return;
+
+    /**
+     * Autofill and hide the whole CAPTCHA stuff in the simple JS mode
+     */
     var $code = jQuery('#plugin__captcha_code');
-    if (!$code.length) return;
+    if ($code.length) {
+        var $box = $wrap.find('input[type=text]');
+        $box.first().val($code.text().replace(/([^A-Z])+/g, ''));
+        $wrap.hide();
+    }
 
-    var $box = jQuery('#plugin__captcha_wrapper input[type=text]');
-    $box.first().val($code.text().replace(/([^A-Z])+/g, ''));
-
-    jQuery('#plugin__captcha_wrapper').hide();
+    /**
+     * Add a HTML5 player for the audio version of the CAPTCHA
+     */
+    var $audiolink = $wrap.find('a');
+    if($audiolink.length) {
+        var audio = document.createElement('audio');
+        if(audio) {
+            audio.src = $audiolink.attr('href');
+            $wrap.append(audio);
+            $audiolink.click(function (e) {
+                audio.play();
+                e.preventDefault();
+                e.stopPropagation();
+            });
+        }
+    }
 });
