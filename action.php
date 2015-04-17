bd26d35b7b32407ed0189c28198eae135056a68b
Andreas Gohr <andi@splitbrain.org>
2015-04-17 21:12:51 +0200

    a bit cleaner way of handling the different modes

--- action.php
+++ action.php
@@ -45,17 +45,54 @@ class action_plugin_captcha extends DokuWiki_Action_Plugin {
     }
 
     /**
+     * Check if the current mode should be handled by CAPTCHA
+     *
+     * @param string $act cleaned action mode
+     * @return bool
+     */
+    protected function is_protected($act) {
+        global $INPUT;
+
+        switch($act) {
+            case 'save':
+                return true;
+            case 'register':
+                return $INPUT->bool('save');
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Aborts the given mode
+     *
+     * Aborting depends on the mode. It might unset certain input parameters or simply switch
+     * the mode to something else (giving as return which needs to be passed back to the
+     * ACTION_ACT_PREPROCESS event)
+     *
+     * @param string $act cleaned action mode
+     * @return string the new mode to use
+     */
+    protected function abort_action($act) {
+        global $INPUT;
+
+        switch($act) {
+            case 'save':
+                return 'preview';
+            case 'register':
+                $INPUT->post->set('save', false);
+                return 'register';
+            default:
+                return $act;
+        }
+    }
+
+    /**
      * Will intercept the 'save' action and check for CAPTCHA first.
      */
     public function handle_captcha_input(Doku_Event $event, $param) {
         $act = act_clean($event->data);
-        if(!(
-            'save' == $act ||
-            ('register' == $act && $_POST['save'])
-        )
-        ) {
-            return; // nothing to do for us
-        }
+        if(!$this->is_protected($act)) return;
 
         // do nothing if logged in user and no CAPTCHA required
         if(!$this->getConf('forusers') && $_SERVER['REMOTE_USER']) {
@@ -66,13 +103,7 @@ class action_plugin_captcha extends DokuWiki_Action_Plugin {
         /** @var helper_plugin_captcha $helper */
         $helper = plugin_load('helper', 'captcha');
         if(!$helper->check()) {
-            if($act == 'save') {
-                // stay in preview mode
-                $event->data = 'preview';
-            } else {
-                // stay in register mode, but disable the save parameter
-                $_POST['save'] = false;
-            }
+            $event->data = $this->abort_action($act);
         }
     }
 
