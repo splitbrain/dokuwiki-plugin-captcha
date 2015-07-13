08846b9b9f8b8a624eec41484d230a42dfbb5cc7
Daniel Raknes <rada@jbv.no>
2015-07-13 14:15:34 +0200

    translation update

--- lang/no/settings.php
+++ lang/no/settings.php
@@ -2,8 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
  * @author Pål Julius Skogholt <pal.julius@skogholt.org>
+ * @author Daniel Raknes <rada@jbv.no>
  */
 $lang['mode']                  = 'Hvilken type CAPTCHA vil du bruke?';
 $lang['mode_o_js']             = 'Tekst (forfylt med JavaScript)';
@@ -14,6 +15,7 @@ $lang['mode_o_image']          = 'Bilde (vanskelig tilgjengelig)';
 $lang['mode_o_audio']          = 'Bilde og lyd (bedre tilgjengelighet)';
 $lang['mode_o_figlet']         = 'Figlet ASCII-kunst (vanskelig tilgjengelig)';
 $lang['forusers']              = 'Bruke CAPTCHA for innlogga brukere?';
+$lang['loginprotect']          = 'Kreve CAPTCHA ved innlogging?';
 $lang['lettercount']           = 'Antall bokstaver (3-16). Om du øker antallet må du også øke bredden av bildet under.';
 $lang['width']                 = 'Bredde på CAPTCHA-bildet (i piksler)';
 $lang['height']                = 'Høyde på CAPTCHA-bildet (i piksler)';
