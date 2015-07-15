6c721f22d33d4e98e83337f0f4aec02c48247333
橙子狼 <2949384951@qq.com>
2015-07-15 21:25:16 +0200

    translation update

--- lang/zh/settings.php
+++ lang/zh/settings.php
@@ -2,9 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author lainme <lainme993@gmail.com>
+ * @author 橙子狼 <2949384951@qq.com>
  */
 $lang['mode']                  = '使用什么类型的验证码？';
 $lang['mode_o_js']             = '文本 (预先由 JavaScript 填写)';
@@ -15,6 +16,7 @@ $lang['mode_o_image']          = '图片 (无障碍性差)';
 $lang['mode_o_audio']          = '图片+音频 (更好的无障碍性)';
 $lang['mode_o_figlet']         = 'Figlet ASCII 艺术 (无障碍性差)';
 $lang['forusers']              = '对已登入的用户也适用吗？';
+$lang['loginprotect']          = '请输入验证码';
 $lang['lettercount']           = '使用字母的数目 (3-16)。如果您增加数目，请确保同时增加图片的宽度。';
 $lang['width']                 = '验证码图片宽度 (像素)';
 $lang['height']                = '验证码图片高度 (像素)';
