4a394576baa3f543fd71eb9e07838c262c43baf7
Jacob Palm <mail@jacobpalm.dk>
2016-06-28 15:10:49 +0200

    translation update

--- lang/da/settings.php
+++ lang/da/settings.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
  * @author soer9648 <soer9648@eucl.dk>
+ * @author Jacob Palm <mail@jacobpalm.dk>
  */
 $lang['mode']                  = 'Hvilken type CAPTCHA skal benyttes?';
 $lang['mode_o_js']             = 'Tekst (præudfyldt af JavaScript)';
@@ -14,6 +15,7 @@ $lang['mode_o_image']          = 'Billede (dårlig tilgængelighed)';
 $lang['mode_o_audio']          = 'Billede+Audio (bedre tilgængelighed)';
 $lang['mode_o_figlet']         = 'Figlet ASCII Art (dårlig tilgængelighed)';
 $lang['forusers']              = 'Benyt også CAPTCHA til brugere der er logget ind?';
+$lang['loginprotect']          = 'Kræv CAPTCHA ved login?';
 $lang['lettercount']           = 'Antal af bogstaver der skal benyttes (3-16). Hvis du øger antallet, skal du også huske at øge bredden af billedet herunder.';
 $lang['width']                 = 'Bredden af CAPTCHA-billedet (pixel)';
 $lang['height']                = 'Højden af CAPTCHA-billedet (pixel)';
