41c32ed77c422e8330531f961f508251125e2bad
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2017-02-14 06:35:05 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -2,11 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
  * @author bruno <bruno@ninesys.fr>
  * @author Fabrice Dejaigher <fabrice@chtiland.com>
  * @author Pietroni <pietroni@informatique.univ-paris-diderot.fr>
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  */
 $lang['mode']                  = 'Quel type de CAPTCHA utiliser ?';
 $lang['mode_o_js']             = 'Texte (prérempli avec JavaScript)';
@@ -15,6 +16,8 @@ $lang['mode_o_math']           = 'Problème mathématique';
 $lang['mode_o_question']       = 'Question fixe';
 $lang['mode_o_image']          = 'Image (mauvaise accessibilité)';
 $lang['mode_o_audio']          = 'Image + Audio (meilleure accessibilité)';
+$lang['mode_o_svg']            = 'SVG (lisible, mauvaise accessibilité)';
+$lang['mode_o_svgaudio']       = 'SVG + Audio (lisible, meilleure accessibilité)';
 $lang['mode_o_figlet']         = 'ASCII Art (mauvaise accessibilité)';
 $lang['forusers']              = 'Utiliser également le CAPTCHA pour les utilisateurs connectés ?';
 $lang['loginprotect']          = 'Exiger un CAPTCHA pour se connecter?';
