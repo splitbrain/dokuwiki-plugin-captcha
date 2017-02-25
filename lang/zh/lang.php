169fe7dd5a83c4be92d74d249a6c72ef3c5b49e8
kuma <kuma000@qq.com>
2017-02-25 13:15:19 +0100

    translation update

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author lainme <lainme993@gmail.com>
  */
--- lang/zh/settings.php
+++ lang/zh/settings.php
@@ -2,10 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author lainme <lainme993@gmail.com>
  * @author 橙子狼 <2949384951@qq.com>
+ * @author kuma <kuma000@qq.com>
  */
 $lang['mode']                  = '使用什么类型的验证码？';
 $lang['mode_o_js']             = '文本 (预先由 JavaScript 填写)';
@@ -14,6 +15,8 @@ $lang['mode_o_math']           = '算术题';
 $lang['mode_o_question']       = '固定问题';
 $lang['mode_o_image']          = '图片 (无障碍性差)';
 $lang['mode_o_audio']          = '图片+音频 (更好的无障碍性)';
+$lang['mode_o_svg']            = 'SVG(不好的无障碍环境,易读的)';
+$lang['mode_o_svgaudio']       = 'SVG +音频(更好的无障碍环境,易读的)';
 $lang['mode_o_figlet']         = 'Figlet ASCII 艺术 (无障碍性差)';
 $lang['forusers']              = '对已登入的用户也适用吗？';
 $lang['loginprotect']          = '请输入验证码';
