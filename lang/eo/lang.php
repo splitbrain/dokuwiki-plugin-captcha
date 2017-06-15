5f34db7b409782f5941f7120df701117481caa99
Moisés Braga Ribeiro <moisesbr@gmail.com>
2017-06-15 02:41:41 -0300

    Review of Esperanto translation

--- lang/eo/lang.php
+++ lang/eo/lang.php
@@ -3,11 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
- * @author Tradukis Ljosxa Kuznecov <ka2pink@gmail.com>
+ * @author Ljosxa Kuznecov <ka2pink@gmail.com>
  * @author Robert Bogenschneider <bogi@uea.org>
+ * @author Moisés Braga Ribeiro <moisesbr@gmail.com>
  */
-$lang['testfailed']            = 'Pardonon, sed CAPTCHA ne respondis korekte. Eble vi tute ne estas homo, ĉu?';
-$lang['fillcaptcha']           = 'Bonvolu tajpi ĉiujn literojn en la kampeton, por pruvi ke vi estas homo.';
-$lang['fillmath']              = 'Bonvolu solvi sekvan ekvacion por pruvi, ke vi estas homa.';
-$lang['soundlink']             = 'Se vi ne povas legi la literojn en la bildo, ŝarĝu tiun .wav-dosieron por aŭdi ilin.';
+$lang['testfailed']            = 'Pardonon, sed la testo CAPTCHA ne estis respondita ĝuste. Eble vi tute ne estas homo, ĉu?';
+$lang['fillcaptcha']           = 'Bonvolu tajpi ĉiujn literojn en la kampon por pruvi ke vi estas homo.';
+$lang['fillmath']              = 'Bonvolu solvi la sekvan ekvacion por pruvi ke vi estas homo.';
+$lang['soundlink']             = 'Se vi ne povas legi la literojn en la bildo, elŝutu tiun dosieron .wav por aŭskulti ilin.';
 $lang['honeypot']              = 'Bonvolu lasi tiun kampon malplena:';
