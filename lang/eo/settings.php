0f4616de90799a2d8efc6546fc5a6573209124a0
Moisés Braga Ribeiro <moisesbr@gmail.com>
2017-06-15 02:41:41 -0300

    Review of Esperanto translation

--- lang/eo/settings.php
+++ lang/eo/settings.php
@@ -3,20 +3,24 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author Tradukis Ljosxa Kuznecov <ka2pink@gmail.com>
+ * @author Ljosxa Kuznecov <ka2pink@gmail.com>
  * @author Robert Bogenschneider <bogi@uea.org>
+ * @author Moisés Braga Ribeiro <moisesbr@gmail.com>
  */
-$lang['mode']                  = 'Kiun varianton de CAPTCHA uzi?';
-$lang['mode_o_js']             = 'Teksto (prilaborita per Java-skripto)';
+$lang['mode']                  = 'Kian teston CAPTCHA uzi?';
+$lang['mode_o_js']             = 'Teksto (antaŭplenumita per Ĝavoskripto)';
 $lang['mode_o_text']           = 'Teksto (nur permane)';
 $lang['mode_o_math']           = 'Matematika problemo';
-$lang['mode_o_question']       = 'Fiksa demando';
+$lang['mode_o_question']       = 'Fiksa Demando';
 $lang['mode_o_image']          = 'Bildo (malbona alirebleco)';
 $lang['mode_o_audio']          = 'Bildo+Sono (pli bona alirebleco)';
-$lang['mode_o_figlet']         = 'Figlet ASCII - arto (malbona alirebleco)';
-$lang['forusers']              = 'Uzi CAPTCHA-n ankaŭ por ensalutintaj uzantoj?';
-$lang['lettercount']           = 'Kvanto da uzendaj literoj (3-16). Se vi pligrandigas la kvanton, certigu ke vi same pligrandigas la larĝecon de la suba bildo.';
-$lang['width']                 = 'Larĝeco de CAPTCHA-bildo (pikseloj)';
-$lang['height']                = 'Alteco de CAPTCHA-bildo (pikseloj)';
-$lang['question']              = 'Demando por fiks-demanda funkciado';
-$lang['answer']                = 'Respondo por fiks-demanda funkciado';
+$lang['mode_o_svg']            = 'SVG (malbona alirebleco, legebla)';
+$lang['mode_o_svgaudio']       = 'SVG+Sono (pli bona alirebleco, legebla)';
+$lang['mode_o_figlet']         = 'Bildo el Askia Arto (malbona alirebleco)';
+$lang['forusers']              = 'Ĉu uzi teston CAPTCHA ankaŭ por ensalutintaj uzantoj?';
+$lang['loginprotect']          = 'Ĉu postuli teston CAPTCHA por ensaluti?';
+$lang['lettercount']           = 'Kvanto da uzendaj literoj (3-16). Se vi pligrandigas la kvanton, certigu ke vi ankaŭ pligrandigas la larĝecon de la suba bildo.';
+$lang['width']                 = 'Larĝeco de la CAPTCHA-bildo (rastrumeroj)';
+$lang['height']                = 'Alteco de la CAPTCHA-bildo (rastrumeroj)';
+$lang['question']              = 'Demando por la reĝimo de fiksa demando';
+$lang['answer']                = 'Respondo por la reĝimo de fiksa demando';
