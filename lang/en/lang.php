1abdd240eaa0c97a41ded082a982015a30fa1fd8
Shawn <stpauljar@yahoo.com>
2020-06-28 14:05:15 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,12 +1,14 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * English language file
  *
  * @author Andreas Gohr <andi@splitbrain.org>
  */
-
-$lang['testfailed']  = "Sorry, but the CAPTCHA wasn't answered correctly. Maybe you're not human at all?";
-$lang['fillcaptcha'] = "Please fill all the letters into the box to prove you're human.";
-$lang['fillmath']    = "Please solve the following equation to prove you're human.";
-$lang['soundlink']   = "If you can't read the letters on the image, download this .wav file to get them read to you.";
-$lang['honeypot']    = "Please keep this field empty: ";
+$lang['testfailed']            = 'Sorry, but the CAPTCHA wasn\'t answered correctly. Maybe you\'re not human at all?';
+$lang['fillcaptcha']           = 'Please fill all the letters into the box to prove you\'re human.';
+$lang['fillmath']              = 'Please solve the following equation to prove you\'re human.';
+$lang['soundlink']             = 'If you can\'t read the letters on the image, download this .wav file to get them read to you.';
+$lang['honeypot']              = 'Please keep this field empty: ';
