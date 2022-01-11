186227361d0efe9f0e20bd52c87dfbf939efb53a
Andreas Gohr <andi@splitbrain.org>
2022-01-11 11:53:15 +0100

    auto reformat code

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -5,8 +5,8 @@
  *
  * @author Andreas Gohr <andi@splitbrain.org>
  */
-$lang['testfailed']            = 'Das CAPTCHA wurde nicht korrekt beantwortet.';
-$lang['fillcaptcha']           = 'Bitte übertragen Sie die Buchstaben in das Eingabefeld.';
-$lang['fillmath']              = 'Bitte lösen Sie folgende Gleichung:';
-$lang['soundlink']             = 'Wenn Sie die Buchstaben auf dem Bild nicht lesen können, laden Sie diese .wav Datei herunter, um sie vorgelesen zu bekommen.';
-$lang['honeypot']              = 'Dieses Feld bitte leer lassen';
+$lang['testfailed'] = 'Das CAPTCHA wurde nicht korrekt beantwortet.';
+$lang['fillcaptcha'] = 'Bitte übertragen Sie die Buchstaben in das Eingabefeld.';
+$lang['fillmath'] = 'Bitte lösen Sie folgende Gleichung:';
+$lang['soundlink'] = 'Wenn Sie die Buchstaben auf dem Bild nicht lesen können, laden Sie diese .wav Datei herunter, um sie vorgelesen zu bekommen.';
+$lang['honeypot'] = 'Dieses Feld bitte leer lassen';
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -8,20 +8,20 @@
  * @author Leo Rudin <leo.rudin@gmx.ch>
  * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  */
-$lang['mode']                  = 'Welcher CAPTCHA-Typ soll benutzt werden?';
-$lang['mode_o_js']             = 'Text (automatisch ausgefüllt via JavaScript)';
-$lang['mode_o_text']           = 'Text (manuell auszufüllen)';
-$lang['mode_o_math']           = 'Mathe-Aufgabe';
-$lang['mode_o_question']       = 'Feste Frage';
-$lang['mode_o_image']          = 'Bild (nicht barrierefrei)';
-$lang['mode_o_audio']          = 'Bild+Audio (barrierefrei)';
-$lang['mode_o_svg']            = 'SVG (nicht barrierefrei, lesbar)';
-$lang['mode_o_svgaudio']       = 'SVG + Audio (barrierefrei, lesbar)';
-$lang['mode_o_figlet']         = 'Figlet ASCII-Kunst (nicht barrierefrei)';
-$lang['forusers']              = 'Soll das CAPTCHA auch für eingeloggte Benutzer gebraucht werden?';
-$lang['loginprotect']          = 'Benötigt es ein CAPTCHA um sich einzuloggen?';
-$lang['lettercount']           = 'Anzahl der zu verwendenen Buchstaben (3-16). Wenn Sie die Anzahl erhöhen, denken Sie daran auch die Breite des Bildes im nächsten Feld zu erhöhen.';
-$lang['width']                 = 'Weite des CAPTCHA Bildes (pixel)';
-$lang['height']                = 'Höhe des CAPTCHA Bildes (pixel)';
-$lang['question']              = 'Frage für den "Feste Frage" Modus.';
-$lang['answer']                = 'Antwort für den "Feste Frage" Modus.';
+$lang['mode'] = 'Welcher CAPTCHA-Typ soll benutzt werden?';
+$lang['mode_o_js'] = 'Text (automatisch ausgefüllt via JavaScript)';
+$lang['mode_o_text'] = 'Text (manuell auszufüllen)';
+$lang['mode_o_math'] = 'Mathe-Aufgabe';
+$lang['mode_o_question'] = 'Feste Frage';
+$lang['mode_o_image'] = 'Bild (nicht barrierefrei)';
+$lang['mode_o_audio'] = 'Bild+Audio (barrierefrei)';
+$lang['mode_o_svg'] = 'SVG (nicht barrierefrei, lesbar)';
+$lang['mode_o_svgaudio'] = 'SVG + Audio (barrierefrei, lesbar)';
+$lang['mode_o_figlet'] = 'Figlet ASCII-Kunst (nicht barrierefrei)';
+$lang['forusers'] = 'Soll das CAPTCHA auch für eingeloggte Benutzer gebraucht werden?';
+$lang['loginprotect'] = 'Benötigt es ein CAPTCHA um sich einzuloggen?';
+$lang['lettercount'] = 'Anzahl der zu verwendenen Buchstaben (3-16). Wenn Sie die Anzahl erhöhen, denken Sie daran auch die Breite des Bildes im nächsten Feld zu erhöhen.';
+$lang['width'] = 'Weite des CAPTCHA Bildes (pixel)';
+$lang['height'] = 'Höhe des CAPTCHA Bildes (pixel)';
+$lang['question'] = 'Frage für den "Feste Frage" Modus.';
+$lang['answer'] = 'Antwort für den "Feste Frage" Modus.';
