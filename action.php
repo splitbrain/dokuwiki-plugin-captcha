31c8e2bd96f192966ad30320d923903ad88fa46e
Andreas Gohr <andi@splitbrain.org>
2022-07-25 13:06:46 +0200

    fix failed login counter. closes #129
    Fixes two problems:
    
    * every request was counted as a failed login because $_REQUEST[u] is
      nearly always set (to an empty string)
    * the captcha entry was displayed one request too late, because the
      event registration happened before the login fail counting

--- action.php
+++ action.php
@@ -31,14 +31,12 @@ class action_plugin_captcha extends DokuWiki_Action_Plugin
         $controller->register_hook('HTML_RESENDPWDFORM_OUTPUT', 'BEFORE', $this, 'handle_form_output', []); //old
         $controller->register_hook('FORM_RESENDPWD_OUTPUT', 'BEFORE', $this, 'handle_form_output', []); //new
 
-        if ($this->protectLogin()) {
-            // inject in login form
-            $controller->register_hook('HTML_LOGINFORM_OUTPUT', 'BEFORE', $this, 'handle_form_output', []); // old
-            $controller->register_hook('FORM_LOGIN_OUTPUT', 'BEFORE', $this, 'handle_form_output', []); // new
+        // inject in login form
+        $controller->register_hook('HTML_LOGINFORM_OUTPUT', 'BEFORE', $this, 'handle_form_output', []); // old
+        $controller->register_hook('FORM_LOGIN_OUTPUT', 'BEFORE', $this, 'handle_form_output', []); // new
 
-            // check on login
-            $controller->register_hook('AUTH_LOGIN_CHECK', 'BEFORE', $this, 'handle_login', []);
-        }
+        // check on login
+        $controller->register_hook('AUTH_LOGIN_CHECK', 'BEFORE', $this, 'handle_login', []);
 
         // clean up captcha cookies
         $controller->register_hook('INDEXER_TASKS_RUN', 'AFTER', $this, 'handle_indexer', []);
@@ -175,6 +173,8 @@ class action_plugin_captcha extends DokuWiki_Action_Plugin
      */
     public function handle_form_output(Doku_Event $event, $param)
     {
+        if (!$this->protectLogin()) return; // no protection wanted
+
         /** @var \dokuwiki\Form\Form|\Doku_Form $form */
         $form = $event->data;
 
@@ -231,7 +231,7 @@ class action_plugin_captcha extends DokuWiki_Action_Plugin
         $act = act_clean($event->data);
         if (
             $act != 'logout' &&
-            $INPUT->has('u') &&
+            $INPUT->str('u') !== '' &&
             empty($INPUT->server->str('http_credentials')) &&
             empty($INPUT->server->str('REMOTE_USER'))
         ) {
