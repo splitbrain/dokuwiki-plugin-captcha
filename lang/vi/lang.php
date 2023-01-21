fe57f49e5f2b2ae6d5c2e22c826cf7f70b0b04e6
Minh <phandinhminh@protonmail.ch>
2023-01-21 22:45:27 +0100

    translation update

--- lang/vi/lang.php
+++ lang/vi/lang.php
@@ -5,8 +5,8 @@
  *
  * @author Thien Hau <thienhau.9a14@gmail.com>
  */
-$lang['testfailed'] = 'Xin lỗi, nhưng CAPTCHA không được trả lời chính xác. Có lẽ bạn không phải là con người?';
-$lang['fillcaptcha'] = 'Vui lòng điền tất cả chữ cái vào hộp để chứng minh bạn là con người.';
-$lang['fillmath'] = 'Hãy giải phương trình sau để chứng minh bạn là con người.';
-$lang['soundlink'] = 'Nếu bạn không thể đọc được chữ cái trên hình ảnh, hãy tải xuống tập tin .wav này để nó đọc cho bạn.';
-$lang['honeypot'] = 'Vui lòng giữ trường này trống:';
+$lang['testfailed']            = 'Xin lỗi, nhưng CAPTCHA không được trả lời chính xác. Có lẽ bạn không phải là con người?';
+$lang['fillcaptcha']           = 'Vui lòng điền tất cả chữ cái vào hộp để chứng minh bạn là con người.';
+$lang['fillmath']              = 'Hãy giải phương trình sau để chứng minh bạn là con người.';
+$lang['soundlink']             = 'Nếu bạn không thể đọc được chữ cái trên hình ảnh, hãy tải xuống tập tin .wav này để nó đọc cho bạn.';
+$lang['honeypot']              = 'Vui lòng giữ trường này trống:';
