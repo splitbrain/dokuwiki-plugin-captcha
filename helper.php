bb63fb25f62ac75462aae93495dbd7e85d8bd5f8
Daniel Dias Rodrigues <danieldiasr@gmail.com>
2023-12-06 05:35:36 -0300

    Fix utf8_strtolower() is deprecated warning (#141)
    Use  \dokuwiki\Utf8\PhpString::strtolower() instead

--- helper.php
+++ helper.php
@@ -150,7 +150,7 @@ class helper_plugin_captcha extends DokuWiki_Plugin
         if (!$field_sec ||
             !$field_in ||
             $rand === false ||
-            utf8_strtolower($field_in) != utf8_strtolower($code) ||
+            \dokuwiki\Utf8\PhpString::strtolower($field_in) != \dokuwiki\Utf8\PhpString::strtolower($code) ||
             trim($field_hp) !== '' ||
             !$this->retrieveCaptchaCookie($this->_fixedIdent(), $rand)
         ) {
